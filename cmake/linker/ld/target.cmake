6b9e020795810fdad058146b01b07eb9f9b50a2a
Mark Ruvald Pedersen <mped@oticon.com>
2019-04-29 23:09:04 +0200

    cmake: rename to toolchain_ld_prebuilt_link

--- cmake/linker/ld/target.cmake
+++ cmake/linker/ld/target.cmake
@@ -208,6 +208,7 @@ macro(toolchain_ld_prebuilt_configure)
 endmacro()
 
 
+macro(toolchain_ld_prebuilt_link)
   # FIXME: Is there any way to get rid of empty_file.c?
   add_executable(       ${ZEPHYR_PREBUILT_EXECUTABLE} misc/empty_file.c)
   target_link_libraries(${ZEPHYR_PREBUILT_EXECUTABLE} ${TOPT} ${PROJECT_BINARY_DIR}/linker.cmd ${PRIV_STACK_LIB} ${zephyr_lnk} ${CODE_RELOCATION_DEP})
